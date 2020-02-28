caf50872f4e6917746b3aeba77416f0a980814a1
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2020-02-28 20:28:13 +0530

    string returns

--- ffi.scm
+++ ffi.scm
@@ -9,9 +9,36 @@
 (addition 12 14)
 
 
-;; Strings
+;; String args
 
 (define count-characters
   (foreign-procedure "count_characters" (string) unsigned-32))
 
-(count-characters "Hello rust!")
+
+(count-characters "Hello World!")
+
+
+;; String returns
+
+(define ptr->string
+  (lambda (ptr)
+    (let loop ((i 0)
+	       (chs '()))
+      (let ((ch (foreign-ref 'char ptr i)))
+	(cond
+	 ((char=? ch #\nul) (list->string (reverse chs)))
+	 (else (loop (+ i 1)
+		     (cons ch chs))))))))
+
+(define song_generate
+  (foreign-procedure "theme_song_generate" (unsigned-8) uptr))
+
+(define song_free
+  (foreign-procedure "theme_song_free" (uptr) void))
+
+(define print-song
+  (lambda ()
+    (let ((s (song_generate 4)))
+      (display (ptr->string s))
+      (newline)
+      (song_free s))))
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use libc::c_char;
 use std::ffi::CStr;
+use std::ffi::CString;
+use std::iter;
 
 #[no_mangle]
 pub extern "C" fn addition(a: u32, b: u32) -> u32 {
@@ -16,4 +18,31 @@ pub extern "C" fn count_characters(s: *const c_char) -> u32 {
 
     let r_str = c_str.to_str().unwrap();
     r_str.chars().count() as u32
+}
+
+// String returns
+
+// Here we use a pair of methods into_raw and from_raw.
+// These convert a CString into a raw pointer that may be passed across the FFI boundary.
+// Ownership of the string is transferred to the caller, but the caller must return the string
+// to Rust in order to properly deallocate the memory.
+
+#[no_mangle]
+pub extern "C" fn theme_song_generate(length: u8) -> *mut c_char {
+    let mut song = String::from("💣 ");
+    song.extend(iter::repeat("na ").take(length as usize));
+    song.push_str("Batman! 💣");
+
+    let c_str_song = CString::new(song).unwrap();
+    c_str_song.into_raw()
+}
+
+#[no_mangle]
+pub extern "C" fn theme_song_free(s: *mut c_char) {
+    unsafe {
+        if s.is_null() {
+            return;
+        }
+        CString::from_raw(s)
+    };
 }
