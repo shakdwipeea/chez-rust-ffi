a9ce0e3d6908d379964ad11bbea997852a17aa20
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2020-02-28 20:18:58 +0530

    string args

--- ffi.scm
+++ ffi.scm
@@ -1,6 +1,17 @@
 (load-shared-object "./target/debug/libchez_ffi.so")
 
+
+;; Integers
+
 (define addition
   (foreign-procedure "addition" (unsigned-32 unsigned-32) unsigned-32))
 
 (addition 12 14)
+
+
+;; Strings
+
+(define count-characters
+  (foreign-procedure "count_characters" (string) unsigned-32))
+
+(count-characters "Hello rust!")
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,19 @@
+use libc::c_char;
+use std::ffi::CStr;
+
 #[no_mangle]
 pub extern "C" fn addition(a: u32, b: u32) -> u32 {
     a + b
+}
+
+#[no_mangle]
+pub extern "C" fn count_characters(s: *const c_char) -> u32 {
+    let c_str = unsafe {
+        assert!(!s.is_null());
+
+        CStr::from_ptr(s)
+    };
+
+    let r_str = c_str.to_str().unwrap();
+    r_str.chars().count() as u32
 }
